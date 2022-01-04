f25236e283a8c3e6a470b7b664a8a5491a410103
Seif Tahawy <seiftahawy54@gmail.com>
2022-01-04 22:55:10 +0200

    fixing removing session error

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const db = require("./utits/db");
-const session = require("express-session");
-const PostgresStore = require("express-pg-session")(session);
-const csrf = require("csurf");
+// const session = require("express-session");
+// const PostgresStore = require("express-pg-session")(session);
+// const csrf = require("csurf");
 
 const app = express();
-const csrfProtection = csrf();
+// const csrfProtection = csrf();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -17,6 +17,7 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+/*
 app.use(
   session({
     secret: "my secret",
@@ -25,6 +26,7 @@ app.use(
     store: PostgresStore,
   })
 );
+*/
 
 const courses = require("./routes/courses");
 const shopping = require("./routes/shopping");
@@ -32,11 +34,11 @@ const auth = require("./routes/auth");
 
 app.use("/courses", courses.routes);
 
-app.use(csrfProtection);
+/*app.use(csrfProtection);
 
 app.use((req, res, next) => {
   res.locals.csrfToken = req.csrfToken();
-});
+});*/
 
 app.use(auth.routes);
 app.use(shopping.routes);
