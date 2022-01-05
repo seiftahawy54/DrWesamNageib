fd95019699f8933cd5c56352ea38cae552dfc186
Seif Tahawy <seiftahawy54@gmail.com>
2022-01-05 16:36:22 +0200

    fixing postgres connection error

--- utits/db.mjs
+++ utits/db.mjs
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new pg.Pool({
-  connectionString: process.env.HEROKU_PGSQL_URI,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
