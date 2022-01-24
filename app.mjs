19543d63dd8e7534d768cf5bd7383d0aa210b44c
Seif Tahawy <seiftahawy54@gmail.com>
2022-01-24 02:41:56 +0200

    fixing package error

--- app.mjs
+++ app.mjs
@@ -4,7 +4,6 @@ import express from "express";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 
-import morgan from "morgan";
 import expressSession from "express-session";
 import pgSession from "connect-pg-simple";
 import csrf from "csurf";
@@ -50,7 +49,6 @@ app.use(cookieParser());
 app.use(
   Multer({ storage: fileStorage, fileFilter: fileFilter }).single("course_img")
 );
-app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.resolve("public")));
 app.use("/uploaded_images", express.static(path.resolve("uploaded_images")));
