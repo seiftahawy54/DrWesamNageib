6c89f1992c277d3199345e29083fd905271285eb
Seif Tahawy <seiftahawy54@gmail.com>
2022-02-25 01:06:25 +0200

    fixing db bugs

--- utits/db.mjs
+++ utits/db.mjs
@@ -6,7 +6,7 @@ dotenv.config();
 
 let sequelize = {};
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === "production") {
   sequelize = new Sequelize(
     process.env.DATABASE_URL,
     // "postgres://dywqkffdboiesh:937a123cb68d20b821a562d1696904fb22070c92294582fd57cc80029b0698d0@ec2-34-236-87-247.compute-1.amazonaws.com:5432/dd25vo8oaccked",
