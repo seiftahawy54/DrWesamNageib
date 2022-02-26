46d31ce1095052a284976ae202b3bb1066f71d74
Seif Tahawy <seiftahawy54@gmail.com>
2022-02-27 01:59:22 +0200

    fixing error hanlder insertion

--- models/errors.mjs
+++ models/errors.mjs
@@ -1,7 +1,14 @@
 import { Sequelize } from "sequelize";
 import { sequelize } from "../utits/db.mjs";
+import { hashCreator } from "../utits/general_helper.mjs";
 
 const Errors = sequelize.define("error", {
+  error_id: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    primaryKey: true,
+    defaultValue: hashCreator(),
+  },
   error: {
     type: Sequelize.TEXT,
     allowNull: false,
--- utits/error_raiser.mjs
+++ utits/error_raiser.mjs
@@ -3,6 +3,6 @@ import { Errors } from "../models/errors.mjs";
 export const errorRaiser = async (err, next) => {
   const error = new Error(err);
   error.httpStatusCode = 500;
-  await Errors.create({ error: JSON.stringify(error) });
+  await Errors.create({ error: JSON.stringify(err) });
   return next(error);
 };
