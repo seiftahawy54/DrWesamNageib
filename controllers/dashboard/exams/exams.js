6d68c21b3215d6c2d2975db0529fb34ef6029e0f
seiftahawy <seiftahway54@gmail.com>
2022-05-21 06:08:50 +0200

    remove exams replies from exams page

--- controllers/dashboard/exams/exams.js
+++ controllers/dashboard/exams/exams.js
@@ -20,29 +20,11 @@ export const getAllExams = async (req, res, next) => {
 
           exam_id = `<span class="link-primary " onclick="copyExamLink(this)" style="cursor: pointer;">/exam/${exam_id}</span>`;
           status = status ? "WORKING" : "CLOSED";
-          if (replies === 0 || !replies) {
-            replies = "No replies.";
-          } else {
-            replies = await Promise.all(
-              replies.map(async ({ user_id, grade }) => {
-                const user_name = await Users.findByPk(user_id, {
-                  attributes: ["name"],
-                });
-                console.log(user_name);
-                return `<span>${user_name.name}: ${grade} </span>`;
-                // return `<span class="chip">${user_id.slice(
-                //   0,
-                //   10
-                // )}, ${grade}</span>`;
-              })
-            );
-          }
 
           return {
             title,
             status,
             exam_id,
-            replies,
           };
         }
       )
@@ -89,10 +71,6 @@ export const getAllExams = async (req, res, next) => {
         {
           title: "Exam Link",
           name: "exam-link",
-        },
-        {
-          title: "No. Replies",
-          name: "exams-answers",
         },
         {
           title: "Update Exam",
