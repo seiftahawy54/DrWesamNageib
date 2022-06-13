49abd5e204aef8c6612745ab2f3856b83023b44d
seiftahawy <seiftahway54@gmail.com>
2022-06-13 22:28:33 +0200

    fixing website bug

--- controllers/auth.js
+++ controllers/auth.js
@@ -335,7 +335,11 @@ export const postRegister = async (req, res, next) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      req.flash("error", errors.array()[0].msg);
+      console.log(errors.array());
+      req.flash(
+        "error",
+        `${errors.array()[0].param} - ${errors.array()[0].msg}`
+      );
       return res.status(422).render("auth/register", {
         title: "Register",
         path: "/register",
