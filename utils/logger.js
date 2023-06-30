4734b51c4cb26948bd8e542bfdb406046772120f
Seif Hesham Salem <seiftahawy54@gmail.com>
2023-06-30 19:18:44 +0300

    fixing logger

--- utils/logger.js
+++ utils/logger.js
@@ -1,34 +1,32 @@
 import winston from "winston";
 import PostgresTransport from "winston-postgres-transport";
 
-// const logger = winston.createLogger({
-//   level: "info",
-//   format: winston.format.json(),
-//   transports: [
-//     //
-//     // - Write all logs with importance level of `error` or less to `error.log`
-//     // - Write all logs with importance level of `info` or less to `combined.log`
-//     //
-//     new PostgresTransport({
-//       postgresUrl: process.env.DATABASE_URL,
-//     }),
-//   ],
-// });
-//
-// if (process.env.NODE_ENV !== "production") {
-//   logger.add(
-//     new winston.transports.Console({
-//       format: winston.format.simple(),
-//     })
-//   );
-// }
-//
-// process.on("unhandledRejection", (reason, promise) => {
-//   // eslint-disable-next-line no-console
-//   console.log("Unhandled Rejection at: Promise", promise, "reason:", reason);
-//   throw reason;
-// });
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  transports: [
+    //
+    // - Write all logs with importance level of `error` or less to `error.log`
+    // - Write all logs with importance level of `info` or less to `combined.log`
+    //
+    new PostgresTransport({
+      postgresUrl: process.env.DATABASE_URL,
+    }),
+  ],
+});
 
-const logger = () => true
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  );
+}
+
+process.on("unhandledRejection", (reason, promise) => {
+  // eslint-disable-next-line no-console
+  console.log("Unhandled Rejection at: Promise", promise, "reason:", reason);
+  throw reason;
+});
 
 export default logger;
