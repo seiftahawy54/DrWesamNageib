4562c0cf5bbc684234e6a90eed6df34b429b1505
Seif Hesham Salem <seiftahawy54@gmail.com>
2023-06-30 20:03:20 +0300

    fixing logger issue

--- utils/logger.js
+++ utils/logger.js
@@ -2,31 +2,30 @@ import winston from "winston";
 import PostgresTransport from "winston-postgres-transport";
 
 const logger = winston.createLogger({
-  level: "info",
-  format: winston.format.json(),
-  transports: [
-    //
-    // - Write all logs with importance level of `error` or less to `error.log`
-    // - Write all logs with importance level of `info` or less to `combined.log`
-    //
-    new PostgresTransport({
-      postgresUrl: process.env.DATABASE_URL,
-    }),
-  ],
+    level: "info",
+    format: winston.format.json(),
+    transports: [
+        //
+        // - Write all logs with importance level of `error` or less to `error.log`
+        // - Write all logs with importance level of `info` or less to `combined.log`
+        //
+        new PostgresTransport({
+            postgresUrl: process.env.DATABASE_URL,
+        }),
+    ],
+    tableName: 'winston_logs',
 });
 
-if (process.env.NODE_ENV !== "production") {
-  logger.add(
+logger.add(
     new winston.transports.Console({
-      format: winston.format.simple(),
+        format: winston.format.simple(),
     })
-  );
-}
+);
 
 process.on("unhandledRejection", (reason, promise) => {
-  // eslint-disable-next-line no-console
-  console.log("Unhandled Rejection at: Promise", promise, "reason:", reason);
-  throw reason;
+    // eslint-disable-next-line no-console
+    console.log("Unhandled Rejection at: Promise", promise, "reason:", reason);
+    throw reason;
 });
 
 export default logger;
